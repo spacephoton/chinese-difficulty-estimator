38331a363b6d0e0d1bda7f22bf19070bff87a3bc
Michael Yared <hi@michael.ninja>
2020-09-26 20:16:02 +0200

    list of items

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef, useState } from "react";
+import "./App.css";
+import Form from "react-bootstrap/Form";
+import ListGroup from "react-bootstrap/ListGroup";
+import Jumbotron from "react-bootstrap/Jumbotron";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
+  const [input, setInput] = useState<string>("");
+  const [characters, setCharacters] = useState([]);
+  const [results, setResults] = useState(<h2>results</h2>);
+
+  const handleChange = (event: any) => {
+    const value = event.target.value;
+    if (typeof value === "string") {
+      setInput(value);
+    }
+  };
+
+  useEffect(() => {
+    const newChars = Array.from(input);
+    const charItems = newChars.map((character) => (
+      <ListGroup.Item>{character}</ListGroup.Item>
+    ));
+    setResults(<ListGroup>{charItems}</ListGroup>);
+  }, [input]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Jumbotron className="center">
+        <h1>Write chinese here:</h1>
+        <Form>
+          <Form.Group controlId="sentence">
+            <Form.Control
+              size="lg"
+              type="text"
+              value={input}
+              onChange={handleChange}
+              placeholder="Enter sentence"
+              maxLength={20}
+              // style={{ maxWidth: "20rem", justifySelf: "center" }}
+            />
+          </Form.Group>
+        </Form>
+        {results}
+      </Jumbotron>
     </div>
   );
 }
