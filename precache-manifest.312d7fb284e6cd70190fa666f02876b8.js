30e78212114ac912fd8927c97b00025b13e6b1f1
Michael Yared <hi@michael.ninja>
2020-09-27 12:36:26 +0200

    Updates

---
+++ precache-manifest.312d7fb284e6cd70190fa666f02876b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb30e0b4d6d8b56806bcbcb438185856",
+    "url": "/index.html"
+  },
+  {
+    "revision": "46fbb9df206eceae3f44",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "5794ab510f61ea524556",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "46fbb9df206eceae3f44",
+    "url": "/static/js/2.a609092a.chunk.js"
+  },
+  {
+    "revision": "c836aaadfe43dfd9fcb8ddeddfd61e18",
+    "url": "/static/js/2.a609092a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5794ab510f61ea524556",
+    "url": "/static/js/main.bf8ba42f.chunk.js"
+  },
+  {
+    "revision": "03a59c09d73e4947bd90",
+    "url": "/static/js/runtime-main.4157d9c8.js"
+  }
+]);
