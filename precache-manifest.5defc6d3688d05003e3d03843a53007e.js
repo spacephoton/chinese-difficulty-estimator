0c06766b15fb25c6ba3f861c48ebf246bb75c5c3
Michael Yared <hi@michael.ninja>
2020-09-27 11:35:58 +0200

    Updates

---
+++ precache-manifest.5defc6d3688d05003e3d03843a53007e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c3e81674f76652304bcd638cccbd51b",
+    "url": "/chinese-difficulty-estimator/index.html"
+  },
+  {
+    "revision": "46fbb9df206eceae3f44",
+    "url": "/chinese-difficulty-estimator/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "243aa53a8afe2e8fe452",
+    "url": "/chinese-difficulty-estimator/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "46fbb9df206eceae3f44",
+    "url": "/chinese-difficulty-estimator/static/js/2.a609092a.chunk.js"
+  },
+  {
+    "revision": "c836aaadfe43dfd9fcb8ddeddfd61e18",
+    "url": "/chinese-difficulty-estimator/static/js/2.a609092a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "243aa53a8afe2e8fe452",
+    "url": "/chinese-difficulty-estimator/static/js/main.24c9a825.chunk.js"
+  },
+  {
+    "revision": "25bdff3c2c56f0f12362",
+    "url": "/chinese-difficulty-estimator/static/js/runtime-main.c0653ecd.js"
+  }
+]);
