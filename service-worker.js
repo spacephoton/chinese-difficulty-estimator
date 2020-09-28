5d3fb3fa1b701f3892efc11cfae4a52863c49832
Michael Yared <hi@michael.ninja>
2020-09-28 22:05:37 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9621bc9270f1e282920c48bd92c2b568.js"
+  "/precache-manifest.43afb71be5186cc1dd6a1d5b4e63e95a.js"
 );
 
 self.addEventListener('message', (event) => {
