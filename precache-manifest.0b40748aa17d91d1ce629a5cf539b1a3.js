c97636f17db85016c7c73a75a09b9529d413e177
Michael Yared <tiankongguangzi@gmail.com>
2021-02-11 12:13:33 +0100

    Updates

---
+++ precache-manifest.0b40748aa17d91d1ce629a5cf539b1a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ff9d5e03929526ba047c0591370a56e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9652752908c2908fe45d",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "73418679e9d6f9bb11e0",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9652752908c2908fe45d",
+    "url": "/static/js/2.3682b18d.chunk.js"
+  },
+  {
+    "revision": "81b6e0f34fc5a80a94fad82949fb96e7",
+    "url": "/static/js/2.3682b18d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73418679e9d6f9bb11e0",
+    "url": "/static/js/main.5b5d74bf.chunk.js"
+  },
+  {
+    "revision": "03a59c09d73e4947bd90",
+    "url": "/static/js/runtime-main.4157d9c8.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f0fcc4a78d79e861b913d3de1fbaa987.js"
+  "/precache-manifest.0b40748aa17d91d1ce629a5cf539b1a3.js"
 );
 
 self.addEventListener('message', (event) => {
